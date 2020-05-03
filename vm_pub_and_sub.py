c78afc217d3c0ffd2f794a75d34d8391b47d828e
LyuTianhua <tianhual@usc.edu email>
2020-05-03 16:49:13 -0700

    MQTT finished

--- vm_pub_and_sub.py
+++ vm_pub_and_sub.py
@@ -1,16 +1,17 @@
 import paho.mqtt.client as mqtt
 import sys
 import time
+import os
 
 #Custom callbacks 
 def screen_brightness_callback(client, userdata, message):
 	print("screen_brightness_callback: " + message.topic + " " + "\"" + 
 		str(message.payload, "utf-8") + "\"")
-	print("screen_brightness_callback: message.payload is of type " + 
-		str(type(message.payload)))
+	screen_brightness_index = str(type(message.payload))
+	print("screen_brightness_callback: message.payload is of type " + screen_brightness_index)
 	# Change brightness here
-	os.system('xbacklight -set ' + str(type(message.payload)))
-
+	os.system('xbacklight -set ' + str(50))
+	
 # Subscribe the screen_brightness topics when connected
 def on_connect(client, userdata, flags, rc):
 	print("Connected to server (i.e., broker) with result code "+str(rc))
@@ -18,6 +19,10 @@ def on_connect(client, userdata, flags, rc):
 	#subscribe to topics of interest here
 	client.subscribe("ubuntu/screen_brightness")
 	client.message_callback_add("ubuntu/screen_brightness", screen_brightness_callback)
+
+#Default message callback.
+def on_message(client, userdata, msg):
+	print("on_message: " + msg.topic + " " + str(msg.payload, "utf-8"))
 
 if __name__ == '__main__':
 
@@ -34,6 +39,6 @@ if __name__ == '__main__':
 	while True:
 		# 1 callback functions for receiving the messages
 		screen_brightness_callback
-		time.sleep(3)
+		time.sleep(1)
 			
 
