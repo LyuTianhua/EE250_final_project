b22ffc18339c1dacdcfa4e32405c75e16fd2daf6
LyuTianhua <tianhual@usc.edu email>
2020-05-04 16:25:15 -0700

    demo finished

--- vm.py
+++ vm.py
@@ -5,48 +5,23 @@ import matplotlib.pyplot as plt
 import paho.mqtt.client as mqtt
 import pandas as pd
 
-cmd = ""
-SPY = False
-WCHN = False
-INDA = False
-
 #Default message callback.
 def on_message(client, userdata, msg):
 	print("on_message: " + msg.topic + " " + str(msg.payload, "utf-8"))
 
 #Custom callbacks 
 def SPY_index_callback(client, userdata, message):
-#	print("SPY_index_callback: " + message.topic + " " + "\"" + 
-#		str(message.payload, "utf-8") + "\"")
-#	dow_index = str(type(message.payload))
-#	print("SPY_index_callback: message.payload is of type " + dow_index)
-	pData = pd.read_json(str(message.payload, "utf-8"));
-	pData = pData.T
-	pData['4. close'].plot()
-	plt.title('SPY index')
-	plt.show()
+	global dataSPYtemp, SPY
+	dataSPYtemp = str(message.payload, "utf-8")
 	
 def WCHN_index_callback(client, userdata, message):
-#	print("nasdaq_index_callback: " + message.topic + " " + "\"" + 
-#		str(message.payload, "utf-8") + "\"")
-#	nasdaq_index = str(type(message.payload))
-#	print("nasdaq_index_callback: message.payload is of type " + nasdaq_index)
-	pData = pd.read_json(str(message.payload, "utf-8"));
-	pData = pData.T
-	pData['4. close'].plot()
-	plt.title('WCHN index')
-	plt.show()
+	global dataWCHNtemp, WCHN
+	dataWCHNtemp = str(message.payload, "utf-8")
 
 def INDA_index_callback(client, userdata, message):
-#	print("sp_index_callback: " + message.topic + " " + "\"" + 
-#		str(message.payload, "utf-8") + "\"")
-#	sp_index = str(type(message.payload))
-#	print("sp_index_callback: message.payload is of type " + sp_index)
-	pData = pd.read_json(str(message.payload, "utf-8"));
-	pData = pData.T
-	pData['4. close'].plot()
-	plt.title('INDA index')
-	plt.show()
+	global dataINDAtemp, INDA
+	dataINDAtemp = str(message.payload, "utf-8")
+	
 	
 # Subscribe the screen_brightness topics when connected
 def on_connect(client, userdata, flags, rc):
@@ -63,35 +38,15 @@ def on_connect(client, userdata, flags, rc):
 	client.subscribe("ubuntu/sp_index")
 	client.message_callback_add("ubuntu/INDA_index", INDA_index_callback)
 
-if __name__ == '__main__':
+cmd = ""
+SPY = False
+WCHN = False
+INDA = False
+dataSPYtemp = ""
+dataWCHNtemp = ""
+dataINDAtemp = ""
 
-	while True:
-		print("\nPlease select the stock index you want to view.\nEnter in numeric order and do not seperate with anything if there are multiple.")
-		print("1-SPDR S&P 500 Trust ETF, 2-WisdomTree ICBCCS S&P China 500 Fund, 3-ISHARES TR/MSCI INDIA ETF")
-		cmd = input("Enter your choice: ")
-		
-		if len(cmd) < 1 | len(cmd) > 3:
-			print("\ninvalid input\n")
-			continue
-			
-		valid = False
-		for elem in cmd:
-			if not(elem.isdigit()):
-				break
-			else:
-				if int(elem) < 1 & int(elem) > 3:
-					break
-				else:
-					valid = True
-					if elem == 1:
-						SPY = True
-					elif elem == 2:
-						WCHN = True
-					elif elem == 3:
-						INDA = True
-		
-		if valid:
-			break;
+if __name__ == '__main__':
 	
 	client = mqtt.Client()
 	client.on_message = on_message
@@ -99,14 +54,90 @@ if __name__ == '__main__':
 	client.connect(host="eclipse.usc.edu", port=11000, keepalive=60)
 	client.loop_start()
 	
+	time.sleep(2)
+	quit = False
+	
 	while True:
-		# callback functions for receiving the messages
+
+		while True:
+			print("\n=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=")
+			print("Please select the stock index you want to view.\nEnter in numeric order and do not seperate with anything if there are multiple.\nEnter \"quit\" to quit is program\n")
+			print("1-SPDR S&P 500 Trust ETF, 2-WisdomTree ICBCCS S&P China 500 Fund, 3-ISHARES TR/MSCI INDIA ETF")
+			cmd = ""
+			cmd = input("Enter your command: ")
+			
+			if cmd == "quit":
+				quit = True
+				break
+			
+			if len(cmd) < 1 | len(cmd) > 3:
+				print("\ninvalid input")
+				print("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=")
+				continue
+				
+			valid = False
+			for elem in cmd:
+				if not(elem.isdigit()):
+					print("\ninvalid input")
+					break
+				else:
+					if int(elem) < 1 | int(elem) > 3:	
+						print("\ninvalid input")
+						break
+					else:
+						valid = True
+						if elem == '1':
+							SPY = True
+						elif elem == '2':
+							WCHN = True
+						elif elem == '3':
+							INDA = True
+			print("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=")
+			if valid:
+				break;
+		
+		if quit:
+			print("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=")
+			break;
+		
+		SPY_index_callback
+		WCHN_index_callback
+		INDA_index_callback
+		
 		if SPY:
-			SPY_index_callback
+			while True:
+				if len(dataSPYtemp) > 0:
+					break
+			dataSPY = pd.read_json(dataSPYtemp)
+			dataSPY = dataSPY.T
+			dataSPY['4. close'].plot()
+			plt.title('SPY index')
+			plt.show()
+			SPY = False
+			
 		if WCHN:
-			WCHN_index_callback
+			while True:
+				if len(dataWCHNtemp) > 0:
+					break
+			dataWCHN = pd.read_json(dataWCHNtemp)
+			dataWCHN = dataWCHN.T
+			dataWCHN['4. close'].plot()
+			plt.title('WCHN index')
+			plt.show()
+			WCHN = False
+			
+
 		if INDA:
-			INDA_index_callback
+			while True:
+				if len(dataINDAtemp) > 0:
+					break
+			dataINDA = pd.read_json(dataINDAtemp)
+			dataINDA = dataINDA.T
+			dataINDA['4. close'].plot()
+			plt.title('INDA index')
+			plt.show()
+			INDA = False
+		
 		time.sleep(1)
 			
 
